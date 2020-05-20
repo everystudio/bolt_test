9d4c01912f1ed942d9ca44994ab3bc33f6f43e57
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-05-20 18:45:50 +0900

    テスト用の動きを見る処理を入れてみた。 なかなかおもしろい

---
+++ Assets/TestBolt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+
+public class TestBolt : MonoBehaviour
+{
+    public bool SampleBoltFunction(int _a)
+    {
+        Debug.Log(_a);
+        return true;
+    }
+
+    public bool TakeDamage( int _iDamage)
+    {
+        Debug.Log(_iDamage);
+        if( 100 <= _iDamage)
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void TestFunc2()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
